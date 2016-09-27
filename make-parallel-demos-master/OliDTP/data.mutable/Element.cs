c7c84dade3082e6c195b5f440d2dd63b07c91923
Markus Seidel <markus@seidel-ob.de>
2016-09-27 20:21:10 +0200

    Initial commit for Basta Examples

---
+++ make-parallel-demos-master/OliDTP/data.mutable/Element.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2010 Oliver Sturm <oliver@oliversturm.com> All rights reserved. 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Data.Mutable {
+  abstract public class Element {
+    public Element() {
+      Source = this;
+    }
+    public PointF Location { get; set; }
+    public SizeF Size { get; set; }
+    public int ZOrder { get; set; }
+    public Element Source { get; set; }
+  }
+}
